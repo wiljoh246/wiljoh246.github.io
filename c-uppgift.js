236e267c7bd3b527640ad14ae94d62b69413d0d3
wiljoh246 <70194203+wiljoh246@users.noreply.github.com>
2020-09-15 09:11:43 +0200

    Add files via upload

---
+++ c-uppgift.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var age = document.getElementById("age").value;
+    var lname = document.getElementById("lname").value;
+    var fname = document.getElementById("fname").value;
+        document.getElementById("demo").innerHTML = "<p>Mitt namn �r " + fname + " " + lname + " och jag �r " + age + " �r gammal</p>";
+    }   
